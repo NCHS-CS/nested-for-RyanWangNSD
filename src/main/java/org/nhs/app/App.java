6697b4e346c9984c7f1a17e7ecd24d912da30b86
Ryan Wang <167727679+RyanWangNSD@users.noreply.github.com>
2024-10-04 21:19:17 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,49 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App {
 
-package org.nhs.app;
+    public static void main (String args[]) {
+        /*
+        for (int i = 1; i <= 5; i++) {
+            for (int x = 1; x <= z; x++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        */
+        // problem 1
+        for (int i = 1; i <= 5; i++) {
+            for (int x = 1; x <= i; x++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
 
-// Your code goes here
+        // problem 2
+        for (int i = 1; i <= 5; i++) {
+            for (int x = 4; x >= i; x--) {
+                System.out.print(" ");
+            }
+            for (int x = 1; x <= i; x++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        // problem 3
+        for (int i = 1; i <= 5; i++) {
+            for (int x = 4; x >= i; x--) {
+                System.out.print(" ");
+            }
+            System.out.println(i);
+        }
+        // challenge problem
+        for (int i = 8; i >= -8; i--) {
+            for (int x = i; x >= 0; x--) {
+                System.out.print(" ");
+            }
+            
+            for (int x = 1; x >= (9-i)*2; x++) {
+                System.out.print(9-i);
+            }
+        }
+    }
+
+}
